1af6359322979e8887bd9b78e05c505eec7a6840
perfect-apex-speed <htet.hw.yan@gmail.com>
2021-11-06 23:08:22 +0800

    add animal years array

--- mixed_messages.js
+++ mixed_messages.js
@@ -12,4 +12,19 @@ let mood;
 
 //Animal Array
 
-let animalArray = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Sheep","Monkey","Rooster","Dog","Pig"];
+const animalArray = ["Rat","Ox","Tiger","Rabbit","Dragon","Snake","Horse","Sheep","Monkey","Rooster","Dog","Pig"];
+
+//Animal Years Array
+
+const ratYearArr = [1924, 1936, 1948, 1960, 1972, 1984, 1996, 2008, 2020];
+const oxYearArr	= [1925, 1937, 1949, 1961, 1973, 1985, 1997, 2009, 2021];
+const tigerYearArr = [1926, 1938, 1950, 1962, 1974, 1986, 1998, 2010, 2022]
+const rabbitYearArr = [1927, 1939, 1951, 1963, 1975, 1987, 1999, 2011, 2023]
+const dragonYearArr = [1928, 1940, 1952, 1964, 1976, 1988, 2000, 2012, 2024]
+const snakeYearArr = [1929, 1941, 1953, 1965, 1977, 1989, 2001, 2013, 2025]
+const horseYearArr = [1930, 1942, 1954, 1966, 1978, 1990, 2002, 2014, 2026]
+const sheepYearArr = [1931, 1943, 1955, 1967, 1979, 1991, 2003, 2015, 2027]
+const monkeyYearArr =  [1932, 1944, 1956, 1968, 1980, 1992, 2004, 2016, 2028]
+const roosterYearArr = [1933, 1945, 1957, 1969, 1981, 1993, 2005, 2017, 2029]
+const dogYearArr = [1934, 1946, 1958, 1970, 1982, 1994, 2006, 2018, 2030]
+const pigYearArr = [1935, 1947, 1959, 1971, 1983, 1995, 2007, 2019, 2031]
